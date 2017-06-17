113a35d6fd6ce4e9920750cdbfd506067c0ebc60
Jason <jzntam@gmail.com>
2017-06-16 18:37:18 -0700

    Lab 1 - Fetch messages from the database and add usernames

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,37 @@
-// Write chatr code here!
+const MessageList = document.getElementById('messages')
+const MessageForm = document.getElementById('new-message')
+
+const Headers = {'Content-Type': 'application/json'}
+
+function getAllMessages() {
+  fetch('/messages')
+    .then(response => response.json())
+    .then(renderMessages)
+}
+
+function createMessage(message) {
+  fetch('/messages', {
+    method: 'POST',
+    body: message
+  })
+}
+
+function renderMessages(messages) {
+  MessageList.innerHTML = messages.map((msg) => {
+    return `
+      <li><strong>${msg.username} says:</strong> ${msg.content}</li>
+    `
+  }).join('')
+}
+
+MessageForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  let formData = new FormData(event.currentTarget)
+
+  createMessage(formData)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  getAllMessages()
+}, false);
